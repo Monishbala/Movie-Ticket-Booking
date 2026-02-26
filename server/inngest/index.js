4a7c7c558cfee2ad839bff6a90736e8f5579813d
Monishbala <monishbeast022@gmail.com>
2026-02-26 15:25:20 +0530

    change 2 in index.js

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -9,7 +9,7 @@ const syncUserCreation = inngest.createFunction(
     {id:"sync-user-from-clerk"},
     {event:"clerk/user.created"},
     async({event})=>{
-        const {id,first_name,last_name,imgae_url,email_addresses}=event.data;
+        const {id,first_name,last_name,image_url,email_addresses}=event.data;
         const userData = {
             _id:id,
             email:email_addresses[0].email_address,
